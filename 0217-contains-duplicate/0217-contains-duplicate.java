b2083daadeefb2cbcda7b40f162b4df76359c138
Shlok Arora <arorashlok2305@gmail.com>
2026-08-15 12:21:57 +0530

    Time: 18 ms (70.05%), Space: 107.7 MB (56.42%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -4,9 +4,8 @@ class Solution {
         for (int num:nums) {
             if (set.contains(num)) {
                 return true;
-            } else {
-                set.add(num);
             }
+            set.add(num);
         }
         return false;
     }
