0dce8dd8d6c5c67ea9a046cacecdf96932a2391f
Shlok Arora <arorashlok2305@gmail.com>
2026-09-12 00:20:25 +0530

    Time: 1 ms (98.45%), Space: 43.7 MB (6.19%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean rotateString(String s, String goal) {
         StringBuilder sb = new StringBuilder(s);
 
-        for (int i = 0; i < s.length(); i++) {
+        for(int i =0 ;i<s.length();i++){
             char temp = sb.charAt(0);
             sb.deleteCharAt(0);
             sb.append(temp);
