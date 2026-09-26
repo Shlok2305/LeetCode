2e66a4de00c933b55cc627782f428851b4a9b3e4
Shlok Arora <arorashlok2305@gmail.com>
2026-09-26 22:59:32 +0530

    Time: 41 ms (39.37%), Space: 90 MB (94.12%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.java
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.java
@@ -1,9 +1,12 @@
 class Solution {
     public String evaluate(String s, List<List<String>> knowledge) {
+        //using StringBuilder so we can update the String
         StringBuilder sb = new StringBuilder();
+        // The secondBuilder is to find the key value
         StringBuilder sb2 = new StringBuilder();
         HashMap<String, String> map = new HashMap<>();
 
+        //add the key values pairs to a HashMap for easy retrival later  
         for (List<String> pair : knowledge) {
             String key = pair.get(0);
             String value = pair.get(1);
@@ -11,19 +14,19 @@ class Solution {
             map.put(key, value);
         }
 
-        int i = 0;
+        int i = 0;//it is a itorator
 
         while (i < s.length()) {
-
-            if (s.charAt(i) == '(') {
+            
+            if (s.charAt(i) == '(') {//if the element is '('so the key is after it so extract chars after that 
                 i++;
 
-                while (s.charAt(i) != ')') {
+                while (s.charAt(i) != ')') {// we only need to find the key after')' are not part of key
                     sb2.append(s.charAt(i));
                     i++;
                 }
 
-                if (map.containsKey(sb2.toString())) {
+                if (map.containsKey(sb2.toString())) {//checking if map contains the key if not appand '?'
                     i++;
                     sb.append(map.get(sb2.toString()));
 
@@ -32,8 +35,8 @@ class Solution {
                     i++;
                 }
 
-                sb2.setLength(0);
-            } else {
+                sb2.setLength(0);//clear for next key
+            } else {//add non key - value pair character
                 sb.append(s.charAt(i));
                 i++;
             }
